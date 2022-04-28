f10b90ccfadb7a1dde7d83f678def33906e2db1d
tatsujiro000 <t.oshikiri000@gmail.com>
2022-04-28 22:19:14 +0900

    first commit

---
+++ functions/index.js
@@ -0,0 +1,36 @@
+const functions = require('firebase-functions');
+//express
+const express = require('express');
+const app = express();
+//cors
+const cors = require('cors')({origin: true});
+
+app.use(cors);
+
+
+app.get('/', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.send(`
+      <!doctype html>
+      <head>
+        <title>Time</title>
+        <link rel="stylesheet" href="/style.css">
+        <script src="/script.js"></script>
+      </head>
+      <body>
+        <p>In London, the clock strikes:
+          <span id="bongs">${'BONG '.repeat(hours)}</span></p>
+        <button onClick="refresh(this)">Refresh</button>
+      </body>
+    </html>`);
+});
+
+app.get('/api', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+});
+
+
+exports.app = functions.https.onRequest(app);
